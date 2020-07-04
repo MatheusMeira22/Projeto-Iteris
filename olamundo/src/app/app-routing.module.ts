35ddb45ec7091e3d63c585d2c0bde75ee1f3b3a6
OBigJones <jpaulo321@gmail.com>
2020-07-04 12:19:19 -0300

    router

--- olamundo/src/app/app-routing.module.ts
+++ olamundo/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NavBarComponent,
+    children: [
+      {
+        path: 'Home',
+        component: CadastroComponent,
+      },
+      {
+      path: 'Cadastro' ,
+      component: CadastroComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
